64dbee56becc0f2ce49bd9b2de82a66c0fbc7349
Keegan Hart <keeganahart@gmail.com>
2021-04-22 19:01:15 -0500

    Suggested Changes and new component
    Added comment to .edgeimg to better explain what it does and created custom row class for the home page. Created new component for the view orders page

--- AngularInventory/src/app/app.module.ts
+++ AngularInventory/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewOrdersComponent } from './vieworders/vieworders.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewOrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'vieworders', component: ViewOrdersComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
