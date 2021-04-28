3ff429554965391da257d4d7920d8da4cb3002d0
Keegan Hart <keeganahart@gmail.com>
2021-04-27 23:30:35 -0500

    Added routing
    Added routing to app

--- AngularInventory/src/app/app.module.ts
+++ AngularInventory/src/app/app.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent} from './homepage/homepage.component'
 import { ViewOrdersComponent } from './vieworders/vieworders.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     ViewOrdersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     RouterModule.forRoot([
-      {path: 'vieworders', component: ViewOrdersComponent}
+      
     ])
   ],
   providers: [],
